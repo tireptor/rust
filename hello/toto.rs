03a148316d10355dec07217da59bbd38bd5b4ee9
Vincent <vincent.lafargue96@gmail.com>
2019-10-26 20:47:27 +0200

    Premier programme

---
+++ hello/toto.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::str::FromStr;
+
+fn main() {
+    println!("Hello world!");
+    let a = 12;
+    let b = 13;
+    let c = a + b;
+    let reponse : Option<isize>;
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    println!("La variable a est égale à : {}", a);
+    println!("Le calcul de {} + {} est égale à {}",a,b,c );
+    reponse =recuperer_entree_utilisateur();
+    println!("")
+}
+fn recuperer_entree_utilisateur() -> Option<isize> { // elle ne prend rien en entrée et retourne un Option<isize> (dans le cas où ça ne fonctionnerait pas)
+    let mut entree = String::new();
+
+    match io::stdin().read_line(&mut entree) { // on récupère ce qu'a entré l'utilisateur dans la variable entree
+        Ok(_) => { // tout s'est bien passé, on peut convertir la String en entier
+            match isize::from_str(&entree.trim()) { // la méthode trim enlève tous les caractères "blancs" en début et fin de chaîne 
+                Ok(nombre) => Some(nombre), // tout s'est bien déroulé, on retourne donc le nombre
+                Err(_) => { // si jamais la conversion échoue (si l'utilisateur n'a pas rentré un nombre valide), on retourne None
+                    println!("Veuillez entrer un nombre valide !");
+                    None
+                }
+            }
+        },
+        _ => { // une erreur s'est produite, on doit avertir l'utilisateur !
+            println!("Erreur lors de la récupération de la saisie...");
+            None
+        }
+    }
+}
